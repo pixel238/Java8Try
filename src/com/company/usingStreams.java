a12db700f61689f5f279bd52f0abea2b71c85769
tavsica-cchowdhary <cchowdhary@tavisca.in>
2019-08-09 15:35:29 +0530

    Added an example of collector in streams

--- src/com/company/usingStreams.java
+++ src/com/company/usingStreams.java
@@ -2,15 +2,33 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class usingStreams {
     public static void main(String [] args)
     {
-        System.out.println("Hello");
-        int array[]={1,2,3,4,5,6,7,8,9,10};
-        Range(1,10).stream().
-                map(x->x).
-                forEach(System.out::println);
+        System.out.println("Table of 2");
+        //int array[]={1,2,3,4,5,6,7,8,9,10};
+        Range(1,10).stream()
+                .map(x->x+"*2="+x*2)
+                .forEach(System.out::println);
+
+        System.out.println("Table of 3");
+        //int array[]={1,2,3,4,5,6,7,8,9,10};
+        List<String> list=Range(1,10).stream()
+                .map(x->x+"*3="+x*3)
+                .collect(Collectors.toList());
+        System.out.println(list);
+
+        System.out.println("Odd no. in range 1 to 10");
+        Range(1,10).stream()
+                .filter(number->isOdd(number))
+                .forEach(System.out::println);
+    }
+
+    public static boolean isOdd(int number)
+    {
+        return (number%2!=0);
     }
 
     public static List<Integer> Range(int left, int right){
